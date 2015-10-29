d5ee7a58a0a2682b0457a34ed57de9f48018014e
Goretti <Goretti@Goretti>
2015-10-29 09:02:52 +0000

    KATA4 - Version2: Muestra el contenido de un directorio
    Lo hace con identacion y se divide en diversos métodos.

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -11,11 +11,25 @@ public class KATA4 {
 
     public static void main(String[] args) {
         
-        String pathName = "c:/";
+        String pathName = "C:\\Users\\Goretti\\Documents";
         File file = new File (pathName);
         String [] dire = file.list();
         for (String dire1 : dire) {
             System.out.println(dire1);
+        }
+        
+        print (file.listFiles(),"");
+    }
+
+    private static void print(File[] listFiles, String ident) {
+        if(listFiles == null){
+            return;
+        }
+        
+        for (File listFile : listFiles) {
+            System.out.println(ident + (listFile.isDirectory()? "+": "#") + listFile.getName());
+            if (listFile.isFile() || listFile.isHidden()) continue;
+            print(listFile.listFiles(), ident + " --> ");
         }
     }
     
