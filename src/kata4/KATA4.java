629c462bcb53254135a3fff362bd6fd6c44007d7
Goretti <Goretti@Goretti>
2015-11-12 10:19:32 +0000

    KATA4: Lee los emails de la base de datos y los añade en el ArrayList para mostrar el histograma

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,15 +1,17 @@
 
 package kata4;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class KATA4 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException, SQLException {
         
-        String nameFile = "C:\\Users\\Goretti\\Documents\\NetBeansProjects\\KATA4\\DATA\\emailsfilev1.txt";
-        ArrayList<String> mailArray = MailListReader.read(nameFile);
+        //String nameFile = "C:\\Users\\Goretti\\Documents\\NetBeansProjects\\KATA4\\DATA\\emailsfilev1.txt";
+        ArrayList<String> mailArray = MailListReaderDDBB.read();
         
         System.out.println(mailArray.size());
         
